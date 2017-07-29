2db71582969ae575e14d2a79a72fff6e3956c2f8
Matt Daniell <mdaniell@me.com>
2017-07-29 22:16:11 +0930

    Scraper development

---
+++ scraper/README.md
@@ -0,0 +1,4 @@
+# Node scraper service
+
+To run
+`sudo node index.js`
---
+++ scraper/index.js
@@ -0,0 +1,24 @@
+
+var phantom = require("phantom");
+var cheerio = require("cheerio");
+var _ph, _page, _outObj;
+
+phantom.create().then(ph => {
+    _ph = ph;
+    return _ph.createPage();
+}).then(page => {
+    _page = page;
+    return _page.open('https://www.waterconnect.sa.gov.au/Systems/SiteInfo/Pages/Default.aspx?site=A5041014&period=HRLY#Real-Time,Real-Time%20Data');
+}).then(status => {
+    console.log(status);
+    return _page.property('content')
+}).then(content => {
+    //console.log(content);
+    var data = {};
+
+    $ = cheerio.load(content);
+    var x = $(".ui-jqgrid-bdiv table").next().html();
+    console.log("CHEERIO", x);
+    _page.close();
+    _ph.exit();
+}).catch(e => console.log(e));
