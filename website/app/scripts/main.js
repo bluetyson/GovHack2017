2cac66882e2a429e7145202256064e9bc4909b3d
Matt Daniell <mdaniell@me.com>
2017-07-30 16:58:04 +0930

    Add holder images for other locations

--- website/app/scripts/main.js
+++ website/app/scripts/main.js
@@ -1,4 +1,14 @@
 
+$('#westlakes .linkhover').on('click',function(){
+  $('html').toggleClass('westlakes-active');
+});
+
 $('#henleybeach .linkhover').on('click',function(){
   $('html').toggleClass('henleybeach-active');
+});
+$('#glenelg .linkhover').on('click',function(){
+  $('html').toggleClass('glenelg-active');
+});
+$('#portnoarlunga .linkhover').on('click',function(){
+  $('html').toggleClass('portnoarlunga-active');
 });
